5ff9d365288adf5210e1bd607d796e721047a188
SilvaEmerson <emersonjose877@gmail.com>
2018-06-09 11:46:49 -0300

    Day :seven: commit

--- Day#7.js
+++ Day#7.js
@@ -1,4 +1,16 @@
-function regexVar() {
-    let re = new RegExp("^([aeiou]).*\\1$", "g");
+
+const regexVar = () => {
+    let re = new RegExp('^([aeiou]).*\\1$', 'g');
     return re;
-}
+};
+
+
+const regexVarMr = () => {
+    // let re = new RegExp('^[MrMrsMsDrEr]', 'g');
+
+    let re = new RegExp('^[aeiou](?=\.|\S)', 'g');
+    return re;
+};
+
+let str = 'e .';
+console.log(str.match(regexVarMr()));
